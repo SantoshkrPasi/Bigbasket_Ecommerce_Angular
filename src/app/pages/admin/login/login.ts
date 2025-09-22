7e69d908d0324f90b7340404f39bc87e2d894109
santosh <santoshpasi6566@gmail.com>
2025-09-22 23:33:23 +0530

    Added Entity and Product Page

--- src/app/pages/admin/login/login.ts
+++ src/app/pages/admin/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-
+ loginObj : any = {
+  userName : '',
+  password : ''
+ }
+ constructor(private router : Router) {}
+ onLogin(){
+  if(this.loginObj.userName == 'admin' && this.loginObj.password == '334455'){
+   this.router.navigateByUrl('/products');
+ }
+ else{
+  alert('Invalid Credentials')
+ }
+}
 }
--- src/app/pages/admin/products/products.ts
+++ src/app/pages/admin/products/products.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule , FormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
+  isSidePanelVisible : boolean = false;
 
+  productObj : any =
+  {
+  "productId": 0,
+  "productSku": "",
+  "productName": "",
+  "productPrice": 0,
+  "productShortName": "",
+  "productDescription": "",
+  "createdDate": new Date(),
+  "deliveryTimeSpan": "",
+  "categoryId": 0,
+  "productImageUrl": ""
+}
+
+  openSidePanel() {   
+      this.isSidePanelVisible = true;
+  }
+  closeSidePanel() {   
+      this.isSidePanelVisible = false;
+  }
 }
